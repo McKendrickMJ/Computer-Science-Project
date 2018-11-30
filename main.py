b36764c645a697cf8ff9ef0c370514f3c30416a9
McKendrickMJ <32634825+McKendrickMJ@users.noreply.github.com>
2018-11-30 12:03:38 +0000

    Update main.py
    Added classes Player, Turret and Mine with logic implemented for player movement. Sprite collisions do not work and prevent invaders from drawing.

--- main.py
+++ main.py
@@ -20,15 +20,19 @@ done = False
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
 
-block_list = pygame.sprite.Group()
+all_sprites_list = pygame.sprite.Group()
+invader_list = pygame.sprite.Group()
 class Invader(pygame.sprite.Sprite):
-    def __init__(self,x,y):
+    def __init__(self,x,y,width,height):
         super().__init__()
-        self.width = 100
-        self.height = 50
+        self.width = width
+        self.height = height
         self.color = RED
         self.x = x
         self.y = y
+        self.image = pygame.Surface([width,height])
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
         
     def move(self):
         self.x = self.x + 1
@@ -36,23 +40,107 @@ class Invader(pygame.sprite.Sprite):
     def draw(self):
         pygame.draw.rect(screen,self.color,(self.x,self.y,self.width,self.height),0)
 
-invader1 = Invader(100,100)
-block_list.add(invader1)
-invader2 = Invader(200,150)
-block_list.add(invader2)
-invader3 = Invader(300,200)
-block_list.add(invader3)
-invader4 = Invader(400,400)
-block_list.add(invader4)
+   # def draw(self):
+    #    pygame.draw.rect(screen,self.color,(self.x,self.y,self.width,self.height),0)
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self,x,y,width,height,speedx,speedy):
+        super().__init__()
+        self.width = width
+        self.height = height
+        self.color = BLACK
+        self.x = x
+        self.y = y
+        self.speedx = speedx
+        self.speedy = speedy
+        self.image = pygame.Surface([width,height])
+        self.image.fill(BLACK)
+        self.rect = self.image.get_rect()
+
+    def draw(self):
+        pygame.draw.rect(screen,self.color,(self.x,self.y,self.width,self.height),0)
+
+class Turret(pygame.sprite.Sprite):
+    def __init__(self,x,y,width,height):
+        super().__init__()
+        self.width = width
+        self.height = height
+        self.color = GREEN
+        self.x = x
+        self.y = y
+        self.image = pygame.Surface([width,height])
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+
+    def draw(self):
+        pygame.draw.rect(screen,self.color,(self.x,self.y,self.width,self.height),0)
+
+class Mine(pygame.sprite.Sprite):
+    def __init__(self,x,y,size):
+        super().__init__()
+        self.size = size
+        self.color = GREEN
+        self.x = x
+        self.y = y
+
+    def draw(self):
+        pygame.draw.rect(screen,self.color,(self.x,self.y,self.width,self.height),0)
+
+
+
+invader1 = Invader(100,100,50,25)
+all_sprites_list.add(invader1)
+invader_list.add(invader1)
+invader2 = Invader(200,150,50,25)
+all_sprites_list.add(invader2)
+invader_list.add(invader2)
+invader3 = Invader(300,200,50,25)
+all_sprites_list.add(invader3)
+invader_list.add(invader3)
+invader4 = Invader(400,400,50,25)
+all_sprites_list.add(invader4)
+invader_list.add(invader4)
+
+turret1 = Turret(300,200,100,50)
+all_sprites_list.add(turret1)
+
+player1 = Player(20,20,20,20,0,0)
+all_sprites_list.add(player1)
+
 # -------- Main Program Loop -----------
 while not done:
     # --- Main event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player1.speedx = -3
+            elif event.key == pygame.K_RIGHT:
+                player1.speedx = 3
+            elif event.key == pygame.K_UP:
+                player1.speedy = -3
+            elif event.key == pygame.K_DOWN:
+                player1.speedy = 3
+
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                player1.speedx = 0
+            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                player1.speedy = 0
+                
+
+    player1.x += player1.speedx
+    player1.y += player1.speedy
+    
+            
  
     # --- Game logic should go here
- 
+
+    # Collisions hit list
+    blocks_hit_list = pygame.sprite.spritecollide(player1, invader_list, True)
+    
     # --- Screen-clearing code goes here
  
     # Here, we clear the screen to white. Don't put other drawing commands
@@ -71,9 +159,21 @@ while not done:
 ##    invader2.move()
 ##    invader3.move()
 ##    invader4.move()
-    for item in block_list:
+    #for item in block_list:
+     #   item.draw()
+      #  item.move()
+
+   # player1.draw()
+
+  #  turret1.draw()
+
+  #  all_sprites_list.draw(screen)
+    for item in all_sprites_list:
         item.draw()
+
+    for item in invader_list:
         item.move()
+
     # --- Drawing code should go here
  
     # --- Go ahead and update the screen with what we've drawn.
